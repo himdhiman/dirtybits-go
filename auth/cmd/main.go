6d3781d380390c69b1641db8baab1209ed50c171
Himanshu Dhiman <himanshudhiman@Himanshus-Air.bbrouter>
2024-04-17 19:46:33 +0530

    bug fixed

--- auth/cmd/main.go
+++ auth/cmd/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/himdhiman/dirtybits-golang/auth/internal/config"
+)
+
+func main() {
+	cfg, err := config.GetConfiguration("/Users/himanshudhiman/code/golang/dirtybits/auth/settings/config.yaml")
+
+	if err != nil {
+		fmt.Errorf("Error occureed: %w", err)
+	}
+
+	fmt.Println(cfg)
+}
--- auth/internal/config/config.go
+++ auth/internal/config/config.go
@@ -1,9 +1,14 @@
 package config
 
 import (
-	"github.com/himdhiman/unified-framework-go/core"
-)
+	"github.com/himdhiman/unified-framework-go/core/config"
+	"github.com/himdhiman/unified-framework-go/core/config/models")
 
-func GetConfiguration() (*core.Config, error) {
-	
+func GetConfiguration(configFilePath string) (*models.Config, error) {
+	config, err := config.LoadConfig(configFilePath, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
 }
